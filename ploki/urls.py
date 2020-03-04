5fd9760bcd38e9eed33fab0bbc137f0efb939599
jta <jtapiovaara@gmail.com>
2020-03-04 19:03:24 +0200

    third Commit added lot of themes and very little anything else.

--- ploki/urls.py
+++ ploki/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.views.generic.dates import ArchiveIndexView
 from . import views
+from .models import Post
 
 app_name = 'ploki'
 
@@ -7,4 +9,5 @@ urlpatterns = [
     path('', views.ploki_index, name='ploki_index'),
     path('<int:pk>/', views.ploki_detail, name='ploki_detail'),
     path('<category>/', views.ploki_category, name='ploki_category'),
+    # path('archive/', ArchiveIndexView.as_view(model=Post, date_field="pub_date"), name="archive"),
 ]
